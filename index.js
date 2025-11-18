1581398b2b5883aa7e9114389de2868d5715e61e
Kuchy777 <jakub.kucharski36@gmail.com>
2025-11-18 09:02:59 +0100

    Com4_1.1

--- index.js
+++ index.js
@@ -3,17 +3,21 @@
 const express = require('express');
 const app = express();
 
-// define endpoint for exercise 1 here
-app.get('/math/circle/:r', (req, res) => {
-//TODO1  
 
+app.get('/math/circle/:r', (req, res) => {
+
+const radius = parseFloat(req.params.r);
+if (isNaN(radius) || radius < 0) {
+    return res.status(400).json({ error: 'Invalid radius' });
+  }
+  const pole = Math.PI * Math.pow(radius, 2);
+  const obwod = 2 * Math.PI * radius;
+  const result = {
+    pole: pole.toFixed(2),
+    obwod: obwod.toFixed(2)
+  };
 
   res.json(result); 
-
-
-
-
-  
 });
 
 //
